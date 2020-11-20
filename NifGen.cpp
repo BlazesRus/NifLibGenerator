4abe43fa83d1be74860382c5aac6243a065a0375
James Michael Armstrong(BlazesRus) <BlazesRus@yahoo.com>
2020-11-19 19:29:16 -0500

    set up generic program using formula tester as base

---
+++ NifGen.cpp
@@ -0,0 +1,17 @@
+// Program execution begins and ends here.
+//
+
+#include <iostream>
+
+#include "AltNum\MediumDec.hpp"
+using MediumDec = BlazesRusCode::MediumDec;
+
+#include "AltNum\AltNumDebug.hpp"
+#include "AltNum\FloatingOperations.hpp"
+#include "Databases\MediumDecFormula.hpp"
+using MediumDecFormula = BlazesRusCode::MediumDecFormula;
+
+int main()
+{
+
+}
